944f5be9bfa7be20bcac6c258bf74c7313f755b9
Lord Vader <leddfoot36@gmail.com>
2018-04-06 21:03:51 +0200

    added some Enzyme component tests, fixed problematic enzyme dependencies and configured jest startup

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,30 +1,8 @@
 import React from 'react';
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import Header from '../../components/Header';
+import { shallow } from 'enzyme';
 
-
-//
-//
-// ENZYME WOULD NOT CONFIGURE!!!!!!
-//
-
-
-
-// import Header from '../../components/Header';
-// import { shallow } from 'enzyme;'
-//
-// test('should render Header correctly', () => {
-//   // const renderer = new ReactShallowRenderer();
-//   // renderer.render(<Header />);
-//   // expect(renderer.getRenderOutput()).toMatchSnapshot();
-//   const wrapper = shallow(<Header />);
-// });
-test ('should setup edit expense action object', () => {
-  const action = editExpense('123abc', {note: 'fake new value'});
-  expect(action).toEqual({
-    type: 'EDIT_EXPENSE',
-    id: '123abc',
-    updates: {
-          note: 'fake new value'
-    }
-  });
+test('should render Header correctly', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
 });
